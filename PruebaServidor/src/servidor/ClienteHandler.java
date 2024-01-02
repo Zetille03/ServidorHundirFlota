255659f154f7f38f78bdbef8e3bc7c21552eafd8
Biblioteca <Biblioteca@DESKTOP-FJ1GK8S>
2024-01-02 20:54:51 +0100

    Commit 02/01/2024

--- PruebaServidor/src/servidor/ClienteHandler.java
+++ PruebaServidor/src/servidor/ClienteHandler.java
@@ -5,97 +5,133 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 public class ClienteHandler implements Runnable {
-    private Socket socketCliente;
-    private String nombre;
-    private ObjectInputStream inputStream;
-    private ObjectOutputStream outputStream;
-    private GestorHundirFlota gestor;
-    
-    private boolean usuarioDuplicado = false;
+	private Socket socketCliente;
+	//Informacion usuario
+	private String nombre;
+	private int id_usuario;
+	private ObjectInputStream inputStream;
+	private ObjectOutputStream outputStream;
+	private GestorHundirFlota gestor;
+	private ArrayList<Partida> partidas = new ArrayList<Partida>();
+	
+	
+	private boolean usuarioDuplicado = false;
 
-    public ClienteHandler(Socket socket, GestorHundirFlota gestor) {
-        this.socketCliente = socket;
-        this.gestor = gestor;
-        
-    }
+	public ClienteHandler(Socket socket, GestorHundirFlota gestor) {
+		this.socketCliente = socket;
+		this.gestor = gestor;
 
-    public String getNombre() {
-        return nombre;
-    }
+	}
 
-    public void enviarMensaje(String mensaje) {
-        try {
-            outputStream.writeObject(mensaje);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    @Override
-    public void run() {
-        try {
-            outputStream = new ObjectOutputStream(socketCliente.getOutputStream());
-            inputStream = new ObjectInputStream(socketCliente.getInputStream());
-        	
-            String mensaje;
-            String[] mensajeArray;
-            mensaje = (String) inputStream.readObject();
-            mensajeArray = mensaje.split("@");
-            mensaje = this.gestor.comprobacionDatosUsuario(mensajeArray[1],mensajeArray[2]);
-            if(mensaje==null) {
-            	this.gestor.enviarMensajeCliente("L@0", this);
-            }else {
-            	nombre = mensajeArray[1];
-            	if(this.gestor.comprobacionConexionMultiple(nombre)==true) {
-            		usuarioDuplicado = true;
-            		this.gestor.enviarMensajeCliente("L@R", this);
-            	}else {
-	            	this.gestor.clientesConectados.put(nombre, this);
-	            	this.gestor.bienvenidaServidor(nombre + " se ha unido al chat.", this);
-	            	System.out.println(mensaje);
-	            	this.gestor.enviarMensajeCliente("L@1@"+mensaje, this);
-            	}
-            }
-            
+	public void enviarMensaje(String mensaje) {
+		try {
+			outputStream.writeObject(mensaje);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-            
-            do {
-            	mensaje = null;
-            	mensajeArray = null;
-                mensaje = (String) inputStream.readObject();
-                if(mensaje.equals("bye")) {
-                	throw new IOException("Cliente se desconecto");
-                }else {
-                	this.gestor.menzajepatos(mensaje, this);
-                }
-            } while (!mensaje.equals("bye"));
-            
-            this.gestor.menzajepatos(nombre + " ha abandonado el chat.", this);
-            if(this.gestor.clientesConectados.containsKey(nombre)) {
-            	this.gestor.clientesConectados.remove(nombre);
-            }
-            this.gestor.clientes.remove(this);
-            socketCliente.close();
-        } catch (IOException | ClassNotFoundException e) {
-        	if(e instanceof SocketException && e.getMessage().equals("Connection reset")) {
-        		System.out.println("Cliente desconectado forzosamente");
-        		this.gestor.menzajepatos(nombre + " ha abandonado el chat.", this);
-        		if(this.gestor.clientesConectados.containsKey(nombre) && usuarioDuplicado == false) {
-                	this.gestor.clientesConectados.remove(nombre);
-                }
-                this.gestor.clientes.remove(this);
-        	} else if(e instanceof IOException && e.getMessage().equals("Cliente se desconecto")) {
-        		System.out.println("Cliente se desconecto");
-        		this.gestor.desconexionServidor(this);
-        		if(this.gestor.clientesConectados.containsKey(nombre) && usuarioDuplicado == false) {
-                	this.gestor.clientesConectados.remove(nombre);
-                }
-                this.gestor.clientes.remove(this);
-        	}else {
-                e.printStackTrace();
-        	}
-        }
-    }
+	@Override
+	public void run() {
+		try {
+			outputStream = new ObjectOutputStream(socketCliente.getOutputStream());
+			inputStream = new ObjectInputStream(socketCliente.getInputStream());
+
+			String mensaje;
+			String[] mensajeArray;
+			mensaje = (String) inputStream.readObject();
+			mensajeArray = mensaje.split("@");
+			mensaje = this.gestor.comprobacionDatosUsuario(mensajeArray[1], mensajeArray[2]);
+			if (mensaje == null) {
+				this.gestor.enviarMensajeCliente("L@0", this);
+			} else {
+				nombre = mensajeArray[1];
+				if (this.gestor.comprobacionConexionMultiple(nombre) == true) {
+					usuarioDuplicado = true;
+					this.gestor.enviarMensajeCliente("L@R", this);
+				} else {
+					this.gestor.clientesConectadosObjetos.put(nombre, this);
+					this.gestor.clientesConectadosID.put(nombre, Integer.parseInt(mensaje));
+					this.id_usuario = Integer.parseInt(mensaje);
+					this.gestor.bienvenidaServidor(nombre + " se ha unido al chat.", this);
+					this.gestor.enviarMensajeCliente("L@1@" + mensaje, this);
+				}
+			}
+
+			do {
+				mensaje = null;
+				mensajeArray = null;
+				mensaje = (String) inputStream.readObject();
+				mensajeArray = mensaje.split("@");
+				if (mensaje.equals("bye")) {
+					throw new IOException("Cliente se desconecto");
+				} else {
+					switch (mensajeArray[0]) {
+					case "P":
+						switch(mensajeArray[1]) {
+						case "N":
+							String hashmapString = this.gestor.clientesEsperandoParaJugar.toString();
+							this.gestor.enviarMensajeCliente("P@H@"+hashmapString, this);
+							this.gestor.clientesEsperandoParaJugar.put(id_usuario, nombre);
+							break;
+						case "C":
+							this.gestor.clientesEsperandoParaJugar.get(id_usuario);
+							break;
+						}
+						break;
+					case "D":
+						System.out.println(mensaje);
+						break;
+					default:
+                    	 System.out.println(mensaje);
+						break;
+					}
+				}
+			} while (!mensaje.equals("bye"));
+
+			this.gestor.menzajepatos(nombre + " ha abandonado el chat.", this);
+			if (this.gestor.clientesConectadosObjetos.containsKey(nombre)) {
+				this.gestor.clientesConectadosObjetos.remove(nombre);
+				this.gestor.clientesConectadosID.remove(nombre);
+			}
+			if(this.gestor.clientesEsperandoParaJugar.containsKey(nombre)) {
+				this.gestor.clientesEsperandoParaJugar.remove(nombre);
+			}
+			this.gestor.clientes.remove(this);
+			socketCliente.close();
+		} catch (IOException | ClassNotFoundException e) {
+			if (e instanceof SocketException && e.getMessage().equals("Connection reset")) {
+				System.out.println("Cliente desconectado forzosamente");
+				this.gestor.menzajepatos(nombre + " ha abandonado el chat.", this);
+				if (this.gestor.clientesConectadosObjetos.containsKey(nombre) && usuarioDuplicado == false) {
+					this.gestor.clientesConectadosObjetos.remove(nombre);
+					this.gestor.clientesConectadosID.remove(nombre);
+				}
+				if(this.gestor.clientesEsperandoParaJugar.containsKey(nombre)) {
+					this.gestor.clientesEsperandoParaJugar.remove(nombre);
+				}
+				this.gestor.clientes.remove(this);
+			} else if (e instanceof IOException && e.getMessage().equals("Cliente se desconecto")) {
+				System.out.println("Cliente se desconecto");
+				this.gestor.desconexionServidor(this);
+				if (this.gestor.clientesConectadosObjetos.containsKey(nombre) && usuarioDuplicado == false) {
+					this.gestor.clientesConectadosObjetos.remove(nombre);
+					this.gestor.clientesConectadosID.remove(nombre);
+				}
+				if(this.gestor.clientesEsperandoParaJugar.containsKey(nombre)) {
+					this.gestor.clientesEsperandoParaJugar.remove(nombre);
+				}
+				this.gestor.clientes.remove(this);
+			} else {
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- PruebaServidor/src/servidor/GestorHundirFlota.java
+++ PruebaServidor/src/servidor/GestorHundirFlota.java
@@ -13,14 +13,21 @@ import java.util.Map;
 public class GestorHundirFlota {
 	//TODO cambiar lista de clientes por hashmap
     public List<ClienteHandler> clientes = new ArrayList<>();
-    public HashMap<String,ClienteHandler> clientesConectados = new HashMap<>();
+    //Hashmap con objeto clase ClienteHandler almacenado
+    public HashMap<String,ClienteHandler> clientesConectadosObjetos = new HashMap<>();
+    //Hashmap con id usuario;
+    public HashMap<String,Integer> clientesConectadosID = new HashMap<>();
+    //Hashmap de los jugadores esperando para jugar
+    public HashMap<Integer,String> clientesEsperandoParaJugar = new HashMap<>();
+    //ArrayList con las partidas
+    public ArrayList<Partida> partidas = new ArrayList<Partida>();
     //Informacion Registro Base de datos
     private String nameDatabase = "hundirflota";
 	private String user = "root";
 	private String pass = "root";
           
     public synchronized void bienvenidaServidor(String mensaje, ClienteHandler remitente) {
-    	for(Map.Entry entry: clientesConectados.entrySet()) {
+    	for(Map.Entry entry: clientesConectadosObjetos.entrySet()) {
     		ClienteHandler cliente = (ClienteHandler) entry.getValue();
     		if(cliente != remitente) {
     			cliente.enviarMensaje(mensaje);;
@@ -34,7 +41,7 @@ public class GestorHundirFlota {
     }
     
     public synchronized void menzajepatos(String mensaje, ClienteHandler remitente) {
-    	for(Map.Entry entry: clientesConectados.entrySet()) {
+    	for(Map.Entry entry: clientesConectadosObjetos.entrySet()) {
     		ClienteHandler cliente = (ClienteHandler) entry.getValue();
     		if(cliente != remitente) {
     			cliente.enviarMensaje(remitente.getNombre() + ": " + mensaje);
@@ -85,7 +92,7 @@ public class GestorHundirFlota {
     }
     
     public synchronized boolean comprobacionConexionMultiple(String nombreUsuario) {
-    	if(clientesConectados.containsKey(nombreUsuario)) {
+    	if(clientesConectadosObjetos.containsKey(nombreUsuario)) {
     		return true;
     	}
     	return false;
